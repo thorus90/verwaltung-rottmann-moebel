bed796af5efeeb678fdf45f9ecb20d04a2c97bae
ddio <ddio@jr0.de>
2014-07-30 18:17:39 +0200

    rename to references

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #!/bin/env python3
 
 symlinks = [
-	(sdir + '/Controller/ReferenzenController.php', cakedir + '/Controller/ReferenzenController.php'),
-	(sdir + '/Model/Referenzen.php', cakedir + '/Model/Referenzen.php'),
-	(sdir + '/View/Referenzen/add.ctp', cakedir + '/View/Referenzen/add.ctp'),
-	(sdir + '/View/Referenzen/index.ctp', cakedir + '/View/Referenzen/index.ctp'),
-	(sdir + '/js/referenzen.js', cakedir + '/webroot/js/referenzen.js'),
+	(sdir + '/Controller/ReferencesController.php', cakedir + '/Controller/ReferencesController.php'),
+	(sdir + '/Model/Reference.php', cakedir + '/Model/Reference.php'),
+	(sdir + '/View/References/add.ctp', cakedir + '/View/References/add.ctp'),
+	(sdir + '/View/References/index.ctp', cakedir + '/View/References/index.ctp'),
+	(sdir + '/js/reference.js', cakedir + '/webroot/js/reference.js'),
 ]
