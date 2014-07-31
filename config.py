67ed36ac93481181b3b41e1db7256d7848184aaa
ddio <ddio@jr0.de>
2014-07-31 16:12:01 +0200

    thumbnail upload working

--- config.py
+++ config.py
@@ -2,7 +2,9 @@
 
 symlinks = [
 	(sdir + '/Controller/ReferencesController.php', cakedir + '/Controller/ReferencesController.php'),
+	(sdir + '/Controller/ReferencePicturesController.php', cakedir + '/Controller/ReferencePicturesController.php'),
 	(sdir + '/Model/Reference.php', cakedir + '/Model/Reference.php'),
+	(sdir + '/Model/ReferencePicture.php', cakedir + '/Model/ReferencePicture.php'),
 	(sdir + '/View/References/add.ctp', cakedir + '/View/References/add.ctp'),
 	(sdir + '/View/References/index.ctp', cakedir + '/View/References/index.ctp'),
 	(sdir + '/js/reference.js', cakedir + '/webroot/js/reference.js'),
